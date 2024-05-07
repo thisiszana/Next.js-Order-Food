28bdb1dd9de59442119c6a61e6fee87a646069a4
Zana Amanallhy <zanaamanallhy80@gmail.com>
2024-05-07 17:26:47 +0330

    Continue debugging ...

--- src/pages/categories/index.js
+++ src/pages/categories/index.js
@@ -11,7 +11,7 @@ export async function getServerSideProps(context) {
     query: { difficulty, time },
   } = context;
 
-  const res = await fetch("http://localhost:4000/data");
+  const res = await fetch(`${process.env.BASE_URL}/data`);
   const data = await res.json();
 
   const filteredData = data.filter((item) => {
